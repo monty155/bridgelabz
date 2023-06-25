48c4804c9ef4fe4130c62ebe4fd7f7c7bc9317d6
Aksh <aksh@Akshs-MacBook-Air.local>
2023-06-25 23:44:44 +0530

    first commit

---
+++ src/employeeWages/Uc2.java
@@ -0,0 +1,17 @@
+package employeeWages;
+
+public class Uc2 {
+    public static void main(String[] args) {
+        int IS_FULL_TIME =1;
+        int EMPLOYEE_RATE_PER_HOUR =10;
+
+        double empcheck = Math.floor((Math.random() *10)% 2);
+
+        if (empcheck == IS_FULL_TIME) {
+            System.out.println("Employee is Present.");
+        }else{
+            System.out.println("Employee is Absent.");
+        }
+    }
+}
+
--- src/practiceProblemsDay2/DisplayUnit.java
+++ src/practiceProblemsDay2/DisplayUnit.java
@@ -2,32 +2,32 @@ package practiceProblemsDay2;
 
 import java.util.Scanner;
 
-public class DisplayUnit {
-    Scanner input = new Scanner(System.in);
-
-    int hundreds = 0;
-    int tens = 0;
-    int ones = 0;
-
-        System.out.println("Enter a 3 digit number: ");
-    int number = input.nextInt();
-
-    hundreds = number / 100;
-        System.out.println("Hundreds place digit: " , hundreds);
-
-    tens = (number - hundreds) / 10;
-        System.out.println("\nTens place digit: " , tens);
-
-
-    ones = (number - tens - hundreds);
-        System.out.println("\nOnes place digit: " , ones);
-
-
-        if (number > 999);
-        System.out.println("\nError! Number more then 3 digits.");
-        if (number < 100);
-        System.out.println("Error! Number less then 3 digits.");
-}
-
-
-
+// = new Scanner(System.in);
+//
+//    int hundreds = 0;
+//    int tens = 0;
+//    int ones = 0;
+//
+//        System..out.println("\nError! Number more then 3 digits.");
+//        if (number < 100);
+//        System.out.println("Error! Number less then 3 digits.");
+//}
+//
+//
+//public class DisplayUnit {
+//    Scanner inputout.println("Enter a 3 digit number: ");
+//    int number = input.nextInt();
+//
+//    hundreds = number / 100;
+//        System.out.println("Hundreds place digit: " , hundreds);
+//
+//    tens = (number - hundreds) / 10;
+//        System.out.println("\nTens place digit: " , tens);
+//
+//
+//    ones = (number - tens - hundreds);
+//        System.out.println("\nOnes place digit: " , ones);
+//
+//
+//        if (number > 999);
+//    System
